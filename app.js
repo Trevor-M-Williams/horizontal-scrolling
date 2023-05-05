7efbc7cf0a75e015b73b0f2f4ff1938535ab19c1
Trevor Williams <tmw7991@gmail.com>
2023-05-05 17:28:19 -0600

    finished

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-const track = document.querySelector(".track");
-const frame = document.querySelector(".frame");
-const items = Array.from(frame.children);
-let frameWidth = 0;
+enableHorizontalScrolling();
+window.onresize = enableHorizontalScrolling;
 
-items.forEach((item) => {
-  const itemWidth = item.offsetWidth;
-  frameWidth += itemWidth;
-});
+function enableHorizontalScrolling() {
+  if (window.innerWidth < 991) {
+    disableHorizontalScrolling();
+    return;
+  }
+  const track = document.querySelector(".track");
+  const frame = document.querySelector(".frame");
+  const items = Array.from(frame.children);
+  let frameWidth = 0;
 
-track.style.height = frameWidth + "px";
+  items.forEach((item) => {
+    const itemWidth = item.offsetWidth;
+    frameWidth += itemWidth;
+  });
+
+  const trackWidth = frameWidth - window.innerWidth + window.innerHeight;
+  track.style.height = trackWidth + "px";
+  const trackOffset = track.getBoundingClientRect().top + window.scrollY;
+
+  handleScroll();
+  window.onscroll = handleScroll;
+
+  function handleScroll() {
+    const scrollY = window.scrollY;
+    const max = trackWidth - window.innerHeight;
+    let shift = scrollY - trackOffset;
+    if (shift < 0) shift = 0;
+    if (shift > max) shift = max;
+    frame.style.transform = `translateX(-${shift}px)`;
+  }
+}
+
+function disableHorizontalScrolling() {
+  const track = document.querySelector(".track");
+  const frame = document.querySelector(".frame");
+  track.style.height = "auto";
+  frame.style.transform = "translateX(0)";
+  window.onscroll = null;
+}
